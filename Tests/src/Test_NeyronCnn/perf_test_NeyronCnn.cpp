d0a9ca7aaae7be18f830b536e50b7d1faecae464
Илья Кочанков <ilakocankov@MacBook-Pro-Ila.Dlink>
2020-02-21 14:16:29 +0300

    Configuration perfomance test files for Sergey Krykov

---
+++ Tests/src/Test_NeyronCnn/perf_test_NeyronCnn.cpp
@@ -0,0 +1,4 @@
+#include <opencv2/ts.hpp>
+#include "Matrix.h"
+#include "NeyronCnn.h"
+#include <fstream>
---
+++ Tests/src/Tests_PerceptronLearning/perf_test_PerceptronLearning.cpp
@@ -0,0 +1,5 @@
+#include "opencv2/ts.hpp"
+#include <fstream>
+#include "PLearns.h"
+#include "Functors.h"
+
