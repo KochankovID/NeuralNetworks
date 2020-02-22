3cee99c7aeca79413f9dc8eebd2eaa4223e4846d
Илья Кочанков <ilakocankov@MacBook-Pro-Ila.Dlink>
2020-02-22 17:32:04 +0300

    some comments

--- ArtificialNN/src/CNN/NeyronCnn.h
+++ ArtificialNN/src/CNN/NeyronCnn.h
@@ -53,22 +53,33 @@ NeyronCnn<T>::NeyronCnn(const int &step_) : Base_Cnn<T>(), step(step_)
 template <typename T>
 Matrix<T> NeyronCnn<T>::Svertka(const Matrix<T> &F, const Matrix<T> &a)
 {
-
+    // Проверка правильности задания шага свертки
 	if ((step > a.getN()) || (step > a.getM()) || (step < 1))
 	{
 		throw NeyronCnnExeption("Задан невозможный шаг свертки!");
 	}
 
+	// Создание результирующей матрицы
 	Matrix<T> rez((a.getN() - F.getN()) / step + 1, (a.getM() - F.getM()) / step + 1);
 
+	// Переменная в которой хранится текущая сумма свертки
 	double sum;
+
+	// Текущая матрица фокуса свертки
 	Matrix<T> fokus;
+
 	for (int i = 0; i < rez.getN(); i++)
 	{
 		for (int j = 0; j < rez.getM(); j++)
 		{
+
+		    // Начало поэлементного умножения
 			sum = 0;
+
+			// Получение текущей подматрицы
 			fokus = a.getPodmatrix(i * step, j * step, F.getN(), F.getM());
+
+			// Вычисление суммы
 			for (int ii = 0; ii < F.getN(); ii++)
 			{
 				for (int jj = 0; jj < F.getM(); jj++)
