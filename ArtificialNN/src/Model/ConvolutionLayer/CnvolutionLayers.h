82f73a47ca710b718753921d507e0df6d889a4b1
Илья Кочанков <ilakocankov@MacBook-Pro-Ila.Dlink>
2020-03-21 10:09:00 +0300

    conv layers add

---
+++ ArtificialNN/src/Model/ConvolutionLayer/CnvolutionLayers.h
@@ -0,0 +1,12 @@
+#ifndef ARTIFICIALNN_CNVOLUTIONLAYERS_H
+#define ARTIFICIALNN_CNVOLUTIONLAYERS_H
+
+#include "ConvolutionLayer.h"
+
+namespace ANN{
+#define D_ConvolutionLayer ConvolutionLayer<double>
+#define F_ConvolutionLayer ConvolutionLayer<float>
+#define I_ConvolutionLayer ConvolutionLayer<int>
+}
+
+#endif //ARTIFICIALNN_CNVOLUTIONLAYERS_H
