3e1ec92d81aff5f8614c4ec3cff8ac92636d32a5
Илья Кочанков <ilakocankov@MacBook-Pro-Ila.Dlink>
2020-04-10 14:37:03 +0300

    tests on dencelayer

--- Tests/src/Test_DenceLayer/test_DenceLayer.cpp
+++ Tests/src/Test_DenceLayer/test_DenceLayer.cpp
@@ -3,6 +3,7 @@
 #include "DenceLayers.h"
 
 using namespace ANN;
+#define MAT_TEST(X,Y) for(size_t iii = 0; iii < X.getN(); iii++){ for(size_t jjj = 0; jjj < X.getM(); jjj++){ EXPECT_EQ(X[iii][jjj], Y); }}
 
 class DenceLayer_methods : public ::testing::Test {
 public:
@@ -200,7 +201,7 @@ TEST_F(DenceLayer_methods, BackPropagation_two_neyrons_Test_works){
     EXPECT_NO_THROW(dence1.BackPropagation(error, in));
 }
 
-TEST_F(DenceLayer_methods, BackPropagation_tow_neyron_Test){
+TEST_F(DenceLayer_methods, BackPropagation_two_neyron_Test){
     // Arrange
     Tensor<double> error(1, 2, 1);
     Tensor<double> in(1,10,1);
@@ -215,3 +216,106 @@ TEST_F(DenceLayer_methods, BackPropagation_tow_neyron_Test){
     EXPECT_EQ(dence1[0][1].GetD(), -6);
 
 }
+
+TEST_F(DenceLayer_methods, BackPropagation_two_neyron_with_derivative_Test){
+    // Arrange
+    Tensor<double> error(1, 2, 1);
+    Tensor<double> in(1,10,1);
+
+    // Act
+    error.Fill(-6);
+    dence1.derivative.Fill(0.5);
+
+    dence1.BackPropagation(error, in);
+    // Assert
+    EXPECT_EQ(dence1[0][0].GetD(), -3);
+    EXPECT_EQ(dence1[0][1].GetD(), -3);
+
+}
+
+TEST_F(DenceLayer_methods, GradDence_Test_works){
+    // Arrange
+    Tensor<double> in(1,10,1);
+    SGD<double> G;
+
+    // Act
+    dence1[0][0].GetD() = 1;
+    dence1[0][1].GetD() = 1;
+
+    // Assert
+    EXPECT_NO_THROW(dence1.GradDes(G, in));
+}
+
+TEST_F(DenceLayer_methods, GradDence_Test){
+    // Arrange
+    Tensor<double> in(1,10,1);
+    SGD<double> G;
+
+    // Act
+    in.Fill(1);
+    dence1[0][0].GetD() = 1;
+    dence1[0][1].GetD() = 1;
+    dence1.GradDes(G, in);
+
+    // Assert
+    MAT_TEST(dence1[0][0], 0);
+    MAT_TEST(dence1[0][1], 0);
+}
+
+TEST_F(DenceLayer_methods, GradDence_not_all_one_Test){
+    // Arrange
+    Tensor<double> in(1,10,1);
+    SGD<double> G;
+
+    // Act
+    in.Fill(1);
+    in[0][0][1] = 0;
+    in[0][0][5] = 0;
+    dence1[0][0].GetD() = 1;
+    dence1[0][1].GetD() = 1;
+    dence1.GradDes(G, in);
+
+    // Assert
+    EXPECT_EQ(dence1[0][0][0][0], 0);
+    EXPECT_EQ(dence1[0][0][0][1], 1);
+    EXPECT_EQ(dence1[0][0][0][2], 0);
+    EXPECT_EQ(dence1[0][0][0][3], 0);
+    EXPECT_EQ(dence1[0][0][0][4], 0);
+    EXPECT_EQ(dence1[0][0][0][5], 1);
+    EXPECT_EQ(dence1[0][0][0][6], 0);
+    EXPECT_EQ(dence1[0][0][0][7], 0);
+    EXPECT_EQ(dence1[0][0][0][8], 0);
+    EXPECT_EQ(dence1[0][0][0][9], 0);
+
+    EXPECT_EQ(dence1[0][1][0][0], 0);
+    EXPECT_EQ(dence1[0][1][0][1], 1);
+    EXPECT_EQ(dence1[0][1][0][2], 0);
+    EXPECT_EQ(dence1[0][1][0][3], 0);
+    EXPECT_EQ(dence1[0][1][0][4], 0);
+    EXPECT_EQ(dence1[0][1][0][5], 1);
+    EXPECT_EQ(dence1[0][1][0][6], 0);
+    EXPECT_EQ(dence1[0][1][0][7], 0);
+    EXPECT_EQ(dence1[0][1][0][8], 0);
+    EXPECT_EQ(dence1[0][1][0][9], 0);
+}
+
+TEST_F(DenceLayer_methods, GradDence_history_Test){
+    // Arrange
+    Tensor<double> in(1,10,1);
+    SGD<double> G(1, 0.5);
+
+
+    // Act
+    in.Fill(1);
+    dence1[0][0].GetD() = 1;
+    dence1[0][1].GetD() = 1;
+    dence1.history[0][0].Fill(2);
+    dence1.history[0][1].Fill(2);
+    dence1.GradDes(G, in);
+
+
+    // Assert
+    MAT_TEST(dence1[0][0], -0.5);
+    MAT_TEST(dence1[0][1], -0.5);
+}
+
