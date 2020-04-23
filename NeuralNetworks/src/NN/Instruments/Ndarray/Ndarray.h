63cf63d5745bf09056b783d22bc1b38c6a3700ab
Илья Кочанков <ilakocankov@MacBook-Pro-Ila.Dlink>
2020-04-23 17:38:49 +0300

    add file

--- NeuralNetworks/src/NN/Instruments/Ndarray/Ndarray.h
+++ NeuralNetworks/src/NN/Instruments/Ndarray/Ndarray.h
@@ -1,8 +1,148 @@
 #ifndef NEURALNETWORKS_NDARRAY_H
 #define NEURALNETWORKS_NDARRAY_H
 
-namespace NN{
+#include <algorithm>
+#include <functional>
+#include <cstdarg>
 
+using std::vector;
+namespace NN{
+    template <typename T>
+    class Ndarray{
+    public:
+        // Конструкторы ---------------------------------
+        Ndarray();
+        explicit Ndarray(vector<size_t> shape);
+        Ndarray(const Ndarray& copy);
+        Ndarray(const Ndarray&& copy);
+
+        // Методы класса --------------------------------
+        // Перегрузки операторов ------------------------
+        T& operator()(int index, ...);
+        const T& operator()(int index, ...) const;
+
+        // Деструктор -----------------------------------
+        ~Ndarray();
+
+        // Класс исключений -----------------------------
+        class NdarrayExeption : public std::logic_error {
+        public:
+            NdarrayExeption(std::string s) : std::logic_error(s) {}
+
+            ~NdarrayExeption() {}
+        };
+
+#ifdef TEST_Ndarray
+    public:
+        // Поля класса ----------------------------------
+        vector<size_t > shape_;
+        T* buffer;
+        size_t size_;
+
+        // Скрытые матоды класса ------------------------
+        void init_buffer();
+#else
+    protected:
+        // Поля класса ----------------------------------
+        vector<size_t > shape_;
+        T* buffer;
+        size_t size_;
+
+        // Скрытые матоды класса ------------------------
+#endif
+    };
+
+    template<typename T>
+    Ndarray<T>::Ndarray() : shape_({0}), buffer(nullptr), size_(0){
+
+    }
+
+    template<typename T>
+    Ndarray<T>::~Ndarray() {
+        delete[] buffer;
+    }
+
+    template<typename T>
+    Ndarray<T>::Ndarray(vector<size_t> shape) : shape_(shape) {
+        init_buffer();
+    }
+
+    template<typename T>
+    Ndarray<T>::Ndarray(const Ndarray &copy) {
+        this->shape_ = copy.shape_;
+        init_buffer();
+        for(size_t i = 0; i < size_; i++){
+            buffer[i] = copy.buffer[i];
+        }
+    }
+
+    template<typename T>
+    void Ndarray<T>::init_buffer() {
+        if((shape_.size() == 1)&&(shape_[0] == 0)){
+            buffer = nullptr;
+            size_ = 0;
+        }else{
+            size_t t = 1;
+            for(auto n : shape_){
+                t *= n;
+            }
+            size_ = t;
+            buffer = new T[t]();
+        }
+    }
+
+    template<typename T>
+    Ndarray<T>::Ndarray(const Ndarray &&copy) {
+
+    }
+
+    template<typename T>
+    T &Ndarray<T>::operator()(int index, ...) {
+        va_list arguments;
+        va_start(arguments, index);
+
+        size_t index_ = 0;
+
+        if((index < 0) || (index >= shape_[0])){
+            throw Ndarray<T>::NdarrayExeption("Wrong index!");
+        }
+        size_t base = size_ / shape_[0];
+        index_ += index * base;
+        for(int i = 1; i < shape_.size(); i++){
+            index = va_arg(arguments, int);
+            if((index < 0) || (index >= shape_[0])){
+                throw Ndarray<T>::NdarrayExeption("Wrong index!");
+            }
+            base /= shape_[i];
+            index_ += index * base;
+        }
+
+        return buffer[index_];
+    }
+
+    template<typename T>
+    const T &Ndarray<T>::operator()(int index, ...) const {
+        va_list arguments;
+        va_start(arguments, index);
+
+        size_t index_ = 0;
+
+        if((index < 0) || (index >= shape_[0])){
+            throw Ndarray<T>::NdarrayExeption("Wrong index!");
+        }
+        size_t base = size_ / shape_[0];
+        index_ += index * base;
+        for(int i = 1; i < shape_.size(); i++){
+            index = va_arg(arguments, int);
+            if((index < 0) || (index >= shape_[0])){
+                throw Ndarray<T>::NdarrayExeption("Wrong index!");
+            }
+            base /= shape_[i];
+            index_ += index * base;
+        }
+
+        return buffer[index_];
+    }
 }
 
 #endif //NEURALNETWORKS_NDARRAY_H
--- Tests/src/Test_Ndarray/test_Ndarray.cpp
+++ Tests/src/Test_Ndarray/test_Ndarray.cpp
@@ -1,2 +1,139 @@
 #include "gtest/gtest.h"
+#define TEST_Ndarray
 #include "Ndarray.h"
+
+using namespace NN;
+
+class Ndarray_Methods : public ::testing::Test {
+public:
+    Ndarray_Methods(): B({3,3,3}) {}
+
+    ~Ndarray_Methods() { /* free protected members here */ }
+
+    void SetUp() {
+        /* called before every test */
+        for(int k = 0; k < 3; k++) {
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    B.buffer[k*6+i*3+j] = 1;
+                }
+            }
+        }
+    }
+    void TearDown() { /* called after every test */ }
+public:
+    Ndarray<double> B;
+};
+class Ndarray_Methods_P : public ::testing::TestWithParam<int> {
+public:
+    Ndarray_Methods_P(): B({3,3,3}) {}
+
+    ~Ndarray_Methods_P() { /* free protected members here */ }
+
+    void SetUp() {
+        /* called before every test */
+        for(int k = 0; k < 3; k++) {
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    B.buffer[k*9+i*3+j] = 1;
+                }
+            }
+        }
+    }
+    void TearDown() { /* called after every test */ }
+public:
+    Ndarray<double> B;
+};
+
+TEST(Ndarray_constructors, default_constructor_works){
+    // Arrange
+
+    // Act
+
+    // Assert
+    EXPECT_NO_THROW(Ndarray<int> ndarray);
+}
+
+TEST(Ndarray_constructors, default_constructor){
+    // Arrange
+    Ndarray<int> ndarray;
+
+    // Act
+
+    // Assert
+    EXPECT_EQ(ndarray.shape_[0], 0);
+    EXPECT_EQ(ndarray.shape_.size(), 1);
+    EXPECT_EQ(ndarray.size_, 0);
+    EXPECT_EQ(ndarray.buffer, nullptr);
+}
+
+TEST(Ndarray_constructors, initializer_constructor_works){
+    // Arrange
+    vector<size_t > v = {1, 2};
+    vector<size_t > v1 = {0};
+
+    // Act
+
+    // Assert
+    EXPECT_NO_THROW(Ndarray<int> ndarray(v));
+    EXPECT_NO_THROW(Ndarray<int> ndarray(v1));
+}
+
+TEST(Ndarray_constructors, initializer_constructor){
+    // Arrange
+    vector<size_t > v = {1, 2};
+    vector<size_t > v1 = {0};
+
+    // Act
+    Ndarray<int> ndarray(v);
+    Ndarray<int> ndarray1(v1);
+
+    // Assert
+    EXPECT_EQ(ndarray.shape_, v);
+    EXPECT_EQ(ndarray.shape_.size(), 2);
+    EXPECT_EQ(ndarray.size_, 2);
+    EXPECT_EQ(ndarray.buffer[0], 0);
+
+    EXPECT_EQ(ndarray1.shape_, v1);
+    EXPECT_EQ(ndarray1.shape_.size(), 1);
+    EXPECT_EQ(ndarray1.size_, 0);
+    EXPECT_EQ(ndarray1.buffer, nullptr);
+}
+
+//TEST(Ndarray_constructors, copy_constructor_works){
+//    // Arrange
+//    Ndarray<int> ndarray({2,3});
+//
+//    // Act
+//
+//    // Assert
+//    EXPECT_NO_THROW(Ndarray<int> ndarray(v));
+//    EXPECT_NO_THROW(Ndarray<int> ndarray(v1));
+//}
+
+TEST_F(Ndarray_Methods, indexation_works){
+    // Arrange
+    // Act
+    // Assert
+    EXPECT_NO_THROW(B(0,0,0));
+}
+
+TEST_P(Ndarray_Methods_P, indexation_all_right){
+    // Arrange
+    // Act
+    // Assert
+    EXPECT_EQ(B(GetParam(),GetParam(),GetParam()), 1);
+}
+
+INSTANTIATE_TEST_CASE_P(
+        indexation_Ndarray,
+        Ndarray_Methods_P,
+        ::testing::Range(1,3)
+        );
+
+TEST_P(Ndarray_Methods_P, indexation_wrong_param){
+    // Arrange
+    // Act
+    // Assert
+    EXPECT_EQ(B(GetParam(),GetParam()), 1);
+}
